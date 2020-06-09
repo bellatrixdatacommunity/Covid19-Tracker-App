43ac4424444ee8ec2a92793dfa7db0c29ee50235
ojasvik <khuranaojasvi@gmail.com>
2020-06-10 02:33:36 +0530

    added daily_cases.dart

---
+++ lib/daily_cases.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DailyCases extends StatefulWidget {
+  @override
+  _DailyCasesState createState() => _DailyCasesState();
+}
+
+class _DailyCasesState extends State<DailyCases> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
